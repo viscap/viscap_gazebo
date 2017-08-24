4f1884e589f562f6afae2f3f3e8ba46aae104465
viscap <grupoviscap@gmail.com>
2017-08-24 17:22:15 -0300

    Slam is now working!

--- src/transform_publisher.cpp
+++ src/transform_publisher.cpp
@@ -1,24 +1,12 @@
-// Esse nó publica dois frames no tf: o frame odom do drone e o frame do laser
+// Esse nó publica o frame do laser no tf
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <nav_msgs/Odometry.h>
 
-
-/*void odomCb(const nav_msgs::OdometryConstPtr& msg){
-  static tf::TransformBroadcaster br;
-  tf::Transform transform;
-  transform.setOrigin( tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z) );
-  transform.setRotation(tf::Quaternion( msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w) );
-  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "odom"));
-}*/
-
 int main(int argc, char** argv){
   ros::init(argc, argv, "transform_publisher");
   ros::NodeHandle nh;
-
-  //ros::Subscriber odomSub = nh.subscribe("/quadrotor_1/state", 1000, &odomCb);
-
 
   tf::TransformListener listener;
 
@@ -35,8 +23,8 @@ int main(int argc, char** argv){
 
     static tf::TransformBroadcaster laserBroadcaster;
     tf::Transform laserTransform;
-    laserTransform.setOrigin( baseTransform.getOrigin() + tf::Vector3(0.0, 0.0, 0.08) );
-    laserTransform.setRotation( baseTransform.getRotation() * tf::Quaternion(1.0, 0.0, 0.0, 0.0) );
+    laserTransform.setOrigin( baseTransform.getOrigin() + tf::Vector3(0.0, 0.0, 0.08) ); // O laser fica 8cm acima da base do quadrotor
+    laserTransform.setRotation( baseTransform.getRotation() * tf::Quaternion(1.0, 0.0, 0.0, 0.0) ); // Rotação de 180 graus em torno do eixo X
     laserBroadcaster.sendTransform(tf::StampedTransform(laserTransform, ros::Time::now(), "/map", "/quadrotor_1/laser0_frame"));
 
     ros::spinOnce();
